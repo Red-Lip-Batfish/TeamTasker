c889c9c0862d6f6cb151937fea50b49be2300a52
Truett <truett.davis@gmail.com>
2023-03-11 12:10:26 -0500

    fix errors again

--- client/index.js
+++ client/index.js
@@ -4,25 +4,24 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './components/App.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-import { Provider } from 'react-redux';
-import store from './store.js';
+import ReactDOM  from 'react-dom/client';
  
 // define our parent component Routing, which will route users to different components based on URI endpoints
 const Routing = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/home' element={ <App />} />
           <Route path='/' element={ <Login />} />
           <Route path='/signup' element={ <Signup />} />
         </Routes>
-      </BrowserRouter>
-    </Provider>
+      </Router>
   );
 }
 
 // hang our app at the root element
-const root = ReactDOM.createRoote(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Routing />);
 
+// there's an error React.createElement type is invalid --> we believe this is because
+// we are importing and using an App component that hasn't beeen defined or exported yet
