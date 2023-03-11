de9062b2e990676e92fe1f4848a77753f5bfc356
Truett <truett.davis@gmail.com>
2023-03-11 09:52:27 -0500

    added login functionality

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,12 +1,37 @@
 // import statements
 import React from 'react';
-import { Link } from "react-router-dom";
+import { navigate, Link } from "react-router-dom";
 
 
 // define Login component
 const Login = (props) => {
   // define onChange and onClick functionality that isn't passed down as a prop
 
+  // define onClick functionality for the login button
+  const login = () => {
+    const username = document.getElementById('usernameInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username, password
+      }),
+    })
+      .then(results => results.json())
+      .then (data => {
+        console.log(data);
+        navigate({
+          pathname: '/home'
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
 
   // render username and password inputs, login and signup buttons
   return(
@@ -14,17 +39,19 @@ const Login = (props) => {
       <h1>Welcome to Log In</h1>
       <input type='text'
         placeholder='Username'
+        id='usernameInput'
         // onChange={e => setNewItem(e.target.value)}
       />
       <input type='text'
         placeholder='Password'
+        id='passwordInput'
       // onChange={e => setNewItem(e.target.value)}
       />
-      <button>Log In</button>
-      <button>Sign Up</button>
+      <button onClick={login}>Log In</button>
+      <span><Link to='/signup'>Sign Up</Link></span>
     </div>
   )
-}
+};
 
 
 export default Login;
