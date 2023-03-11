121ac0b53b9040b9aa4499a3f6e7f8fee356b102
Truett <truett.davis@gmail.com>
2023-03-11 14:01:52 -0500

    added signup functionality and server request debugging

--- client/components/Signup.jsx
+++ client/components/Signup.jsx
@@ -1,10 +1,35 @@
 // import statements/
 import React from 'react';
-import { Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 
 // define Signup component
 const Signup = (props) => {
-  // define onChange and onClick functionality that isn't passed down as a prop
+  // define onClick functionality for the signup function
+  const signupAndLogin = () => {
+    const username = document.getElementById('signupUsername').value;
+    const password = document.getElementById('signupPassword').value;
+
+    fetch('/signup', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username, password
+      }),
+    })
+      .then(results => results.json())
+      .then (data => {
+        console.log(data);
+        navigate({
+          pathname: '/home',
+          search: `?username=${username}`,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
 
   // render signup fields and buttons
   return (
@@ -12,16 +37,18 @@ const Signup = (props) => {
       <h1>Please Sign Up</h1>
       <input type='text'
           placeholder='Username'
+          id='signupUsername'
       // onChange={e => setNewItem(e.target.value)}
       />
       <input type='text'
           placeholder='Password'
+          id='signupPassword'
       // onChange={e => setNewItem(e.target.value)}
       />
-      <button>Log In</button>
-      <button>Sign Up</button>
+      <button onClick={signupAndLogin}>Sign Up</button>
+      <span><Link to='/'>Already have an account? Log In</Link></span>
     </div>
   )
-}
+} 
 
 export default Signup;
